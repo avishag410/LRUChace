d9b82862bf12bd1980ce6b2136388a885683da37
Avishag <avishag212@gmail.com>
2024-04-30 23:36:27 +0300

    complete code base with running example

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LRUChace
+changes from inital commit to last version:
+
+- change pydantic model to class
+- prev & next save the actual node instead of a copy of the key
+- create utils functions: _add_to_end, _move_to_end, _pop_first & reuse them in the put & get functions
+- no need to hold a counter, just run len(cache)
+- in the function get() - return the item itself (if not exists return None)
+- in put() function - handle another edge case - put(key) where key is already exists in the cache
+
+# how to run?
+python lrucache.py
--- lrucache.py
+++ lrucache.py
@@ -1,55 +1,86 @@
+class Item:
+    def __init__(self, key):
+        self.key = key
+        self.prev = None
+        self.next = None
 
-class chainItem(BaseModel):
-    # key object: int
-    next: int 
-    prev: int
+# Initialize variables
+cache = {}
+first_item = None  # Least recently used item (LRU)
+last_item = None   # Most recently used item (MRU)
+MAX_SIZE = 3
 
-chain = {
-}
+def _add_to_end(item):
+    """Add new item to the end (most recently used)"""
+    global first_item, last_item
+    # initialize the item
+    item.next = None
+    item.prev = last_item
 
-last_item = None
-first_item = None
-MAX = 3
-counter = 0
+    # update last_item pointer
+    if last_item:
+        last_item.next = item
+    last_item = item
 
-def _is_max():
-    counter == MAX
-
-def put(object: int):
-        
-    # enter a new object
-    chain[object] = {
-        prev: last_item if last_item else None  
-        next: None
-    }
-
-    last_item = object
-    counter += 1
-
+    # if it's the first item in the cache
     if first_item is None:
-        first_item = object
+        first_item = item
 
-    if _is_max():
-        next_first_item = chain[first_item].next
-        chain[first_item].pop()
-        
-        first_item = next_first_item
+def _remove_item(item):
+    """Remove an item from the linked list"""
+    global first_item, last_item
+    if item.prev:
+        item.prev.next = item.next
+    
+    ## if item is the first in the cache (has no prev) 
+    else:
+        first_item = item.next
+    if item.next:
+        item.next.prev = item.prev
 
-def get(object: int): 
-    # init
-    prev_item = chain[object].prev
-    next_item = chain[object].next
+    ## if item is the last in the cache (has no next) 
+    else:
+        last_item = item.prev
 
-    # handle prev and next items
-    chain[prev_item].next = next_item
-    chain[next_item].prev = prev_item
+def _move_to_end(item):
+    """Move certain item to the end (most recently used)"""
+    if item == last_item:
+        return
+    _remove_item(item)
+    _add_to_end(item)
 
-    # insert object to the end of the chain
-    chain[object].prev = last_item
-    chain[last_item].next = object
-    chain[object].next = None
+def _pop_first():
+    """Pop the least recently used item from the beginning"""
+    global first_item, cache
+    if first_item:
+        popped_item = first_item
+        _remove_item(first_item)
+        del cache[popped_item.key]
+        return popped_item
 
-    last_item = object
-    if object == first_item:
-        first_item = next_item
+def put(key):
+    global cache, MAX_SIZE
+    item = cache.get(key, None)
+    if not item:
+        new_item = Item(key=key)
+        cache[key] = new_item
+        _add_to_end(new_item)
+        # if reached the maximun capacity of the cache
+        if len(cache) > MAX_SIZE:
+            _pop_first()
+    else:
+        _move_to_end(item)
 
+def get(key):
+    item = cache.get(key, None)
+    if item:
+        _move_to_end(item)
+    return item
+
+# Example usage
+put(1)
+put(2)
+put(3)
+print(get(2).key)  # Accessing 2, should now be the most recently used
+put(4)             # 1 should be evicted here because it's the least recently used
+print(get(1))      # Trying to access 1, should return None
